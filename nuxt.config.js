50d821261d3b7f6eb158bd037a4d9d01034c1833
Vincent Lau <lauchunyin@gmail.com>
2017-04-28 15:26:14 +0800

    update og

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' },
-      { property: 'og:image', content: '/timeis.ltd.jpg' }
+      { property: 'og:image', content: '/timeis.ltd.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
