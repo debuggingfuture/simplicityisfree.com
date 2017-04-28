3c72d32fac7234f72eded93a9ce69064e0a841a4
Vincent Lau <lauchunyin@gmail.com>
2017-04-28 15:26:14 +0800

    update og

--- .nuxt/App.vue
+++ .nuxt/App.vue
@@ -15,7 +15,7 @@ let layouts = {
 }
 
 export default {
-  head: {"title":"Time is Limited","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Nuxt.js project"},{"property":"og:image","content":"/timeis.ltd.jpg"}],"link":[{"rel":"icon","type":"image/x-icon","href":"/favicon.ico"},{"rel":"stylesheet","href":"https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif"}],"script":[{"src":"//cdn.embedly.com/widgets/platform.js"}]},
+  head: {"title":"Time is Limited","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Nuxt.js project"},{"property":"og:image","content":"/timeis.ltd.png"}],"link":[{"rel":"icon","type":"image/x-icon","href":"/favicon.ico"},{"rel":"stylesheet","href":"https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif"}],"script":[{"src":"//cdn.embedly.com/widgets/platform.js"}]},
   data: () => ({
     layout: null,
     layoutName: ''
